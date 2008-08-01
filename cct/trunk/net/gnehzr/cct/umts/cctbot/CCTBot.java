005bbff4b0347e6f8dc3daf206bed386559e972f
jeremy <jeremy@df644910-1a55-0410-bbcf-cf9631938506>
2008-08-01 00:09:18 +0000

    Added stdin interface for cctbot.

--- cct/trunk/net/gnehzr/cct/umts/cctbot/CCTBot.java
+++ cct/trunk/net/gnehzr/cct/umts/cctbot/CCTBot.java
@@ -1,9 +1,12 @@
 package net.gnehzr.cct.umts.cctbot;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import net.gnehzr.cct.scrambles.ScramblePlugin;
 import net.gnehzr.cct.scrambles.ScrambleVariation;
@@ -13,11 +16,7 @@ import org.jibble.pircbot.NickAlreadyInUseException;
 import org.jibble.pircbot.PircBot;
 
 public class CCTBot extends PircBot {
-	//TODO - some sort of logging mechanism might be nice here - log4j?
-	//TODO - some way to get the bot to join other channels? easy, but risky...
-	//TODO - query for scrambles
 	//TODO - add generator support
-	//TODO - refresh scramble plugins
 	public CCTBot() {}
 	
 	//max message length: 470 characters
@@ -47,28 +46,165 @@ public class CCTBot extends PircBot {
 				}
 			} else
 				sendMessage(channel, "Couldn't find scramble variation corresponding to: " + varAndCount[0] + ". " +
-						"Available variations: " + Arrays.toString(ScramblePlugin.getScrambleVariations()));
+						getAvailableVariations());
 		}
 	}
 	
 	public void log(String line) {
-		System.out.println(line);
+		if(!isConnected())
+			System.out.println(line);
+	}
+
+	private static final HashMap<String, String> commands = new HashMap<String, String>();
+	private static final String CMD_RELOAD = "reload";
+	{
+		commands.put(CMD_RELOAD, "Reloads scramble plugins from directory.");
+	}
+		private static final String CMD_LSVARIATIONS = "variations";
+	{
+		commands.put(CMD_LSVARIATIONS, "Prints available variations");
+	}
+	private static final String CMD_CHANNELS = "channels";
+	{
+		commands.put(CMD_CHANNELS, "Prints channels cctbot is connected to.");
+	}
+	private static final String CMD_JOIN = "join";
+	{
+		commands.put(CMD_JOIN, "join #CHANNEL\n\tAttempts to join the specified channel");
+	}
+	private static final String CMD_PART = "part";
+	{
+		commands.put(CMD_PART, "part #CHANNEL\n\tLeaves the specified channel");
+	}
+	private static final String CMD_NICK = "nick";
+	{
+		commands.put(CMD_NICK, "nick NEWNICK\n\tChanges cctbots nickname.");
+	}
+	private static final String CMD_QUIT = "quit";
+	{
+		commands.put(CMD_QUIT, "Disconnects from server and shuts down cctbot.");
+	}
+	private static final String CMD_HELP = "help";
+	{
+		commands.put(CMD_HELP, "help (COMMAND)\n\tPrints available variations");
+	}
+	//TODO - refresh scramble plugins
+	//TODO - some way to get the bot to join other channels? easy, but risky...
+	private void readEvalPrint() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		while(true) {
+			printPrompt();
+			String line = in.readLine();
+			
+			String[] commandAndArg = line.trim().split(" +", 2);
+			String command = commandAndArg[0];
+			String arg = commandAndArg.length == 2 ? commandAndArg[1] : null;
+			if(command.equalsIgnoreCase(CMD_HELP)) {
+				if(arg != null) {
+					String usage = commands.get(arg);
+					System.out.println(usage == null ? "Command " + arg + " not found." : "USAGE: " + usage);
+				}
+				if(arg == null) {
+					StringBuilder cmds = new StringBuilder();
+					for(String c : commands.keySet())
+						cmds.append(", ").append(c);
+					System.out.println("Available commands:\n\t" + cmds.substring(2));
+				}
+				continue;
+			} else if(command.equalsIgnoreCase(CMD_RELOAD)) {
+				System.out.println("Reloading scramble plugins...");
+				ScramblePlugin.clearScramblePlugins();
+				System.out.println(getAvailableVariations());
+				continue;
+			} else if(command.equalsIgnoreCase(CMD_CHANNELS)) {
+				System.out.println("Connected to: " + Arrays.toString(getChannels()));
+				continue;
+			} else if(command.equalsIgnoreCase(CMD_JOIN)) {
+				if(arg != null && arg.startsWith("#")) {
+					System.out.println("Attempting to join " + arg);
+					joinChannel(arg);
+					continue;
+				}
+			} else if(command.equalsIgnoreCase(CMD_PART)) {
+				if(arg != null && arg.startsWith("#")) {
+					System.out.println("Leaving " + arg);
+					partChannel(arg);
+					continue;
+				}
+			} else if(command.equalsIgnoreCase(CMD_NICK)) {
+				if(arg != null) {
+					changeNick(arg);
+					continue;
+				}
+			} else if(command.equalsIgnoreCase(CMD_LSVARIATIONS)) {
+				System.out.println(getAvailableVariations());
+				continue;
+			} else if(command.equalsIgnoreCase(CMD_QUIT)) {
+				shuttingdown = true;
+				quitServer();
+				System.out.println("Exiting cctbot");
+				System.exit(0);
+				continue;
+			}
+			
+			String usage = commands.get(command);
+			System.out.println(usage == null ? "Unrecognized command: " + command + ". Try help." : "USAGE: " + usage);
+		}
 	}
 	
+	private boolean shuttingdown = false;
 	protected void onDisconnect() {
-		while(!isConnected()) {
-		    try {
-		        reconnect();
-		    } catch(Exception e) {
-		        // Couldn't reconnect!
-		        // Pause for a short while...?
-		    	try {
+		while(!isConnected() && !shuttingdown) {
+			try {
+				reconnect();
+			} catch(Exception e) {
+				// Couldn't reconnect!
+				// Pause for a short while...?
+				try {
 					Thread.sleep(1000);
-				} catch (InterruptedException e1) {}
-		    }
+				} catch(InterruptedException e1) {}
+			}
+		}
+	}
+	
+	private void printPrompt() {
+		System.out.print("cctbot: ");
+	}
+	
+	protected void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {
+		if(recipientNick.equals(getNick())) {
+			System.out.println("You have been kicked from " + channel + " by " + kickerNick);
+			printPrompt();
 		}
 	}
 
+	protected void onPart(String channel, String sender, String login, String hostname) {
+		if(sender.equals(getNick())) {
+			System.out.println("You have parted " + channel);
+			printPrompt();
+			printPrompt();
+		}
+	}
+
+	protected void onJoin(String channel, String sender, String login, String hostname) {
+		if(sender.equals(getNick())) {
+			System.out.println("You have joined " + channel);
+			printPrompt();
+		}
+	}
+
+	protected void onNickChange(String oldNick, String login, String hostname, String newNick) {
+		if(newNick.equals(getNick())) {
+			System.out.println("You (formerly: " + oldNick + ") are now known as " + newNick);
+			printPrompt();
+		}
+	}
+	
+	private String getAvailableVariations() {
+		return "Available variations: " + Arrays.toString(ScramblePlugin.getScrambleVariations());
+	}
+	
 	private static void printUsage() {
 		System.out.println("USAGE: CCTBot irc://servername.tld(:port)#channel");
 	}
@@ -104,6 +240,8 @@ public class CCTBot extends PircBot {
 			else
 				cctbot.connect(u.getHost(), u.getPort());
 			cctbot.joinChannel("#" + u.getFragment());
+			
+			cctbot.readEvalPrint();
 		} catch (NickAlreadyInUseException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -111,5 +249,6 @@ public class CCTBot extends PircBot {
 		} catch (IrcException e) {
 			e.printStackTrace();
 		}
+		
 	}
 }
